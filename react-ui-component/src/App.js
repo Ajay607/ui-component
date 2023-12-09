644515f921cb60002502c8a837b79cd53404c694
CodeNinjaAkash <your.email@example.com>
2023-12-10 00:43:03 +0530

    pagination added

--- react-ui-component/src/App.js
+++ react-ui-component/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import Accordion from './component/Accordion/Accordion';
+// import Accordion from './component/Accordion/Accordion';
 import ShowmModal from './component/Modal/ShowmModal';
+// import CustomPagination from './component/customPagination/CustomPagination';
 
 function App() {
   return (
     <div className="App">
       {/* <Accordion /> */}
       <ShowmModal />
+      {/* <CustomPagination /> */}
     </div>
   );
 }
